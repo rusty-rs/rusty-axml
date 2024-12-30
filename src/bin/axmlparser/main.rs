4d183fcf0cb52473eec9e4d3629a8c55976c3177
Julien Gamba <julien@jgamba.eu>
2024-12-30 23:11:26 +0100

    Parse files from APK or AXML. ARSC still todo.

--- src/bin/axmlparser/main.rs
+++ src/bin/axmlparser/main.rs
@@ -1,18 +1,28 @@
 #![cfg(feature = "cli")]
 pub mod cli;
 
-use rusty_axml::create_cursor_from_apk;
+use rusty_axml::{
+    create_cursor_from_apk,
+    create_cursor_from_axml,
+};
 use rusty_axml::parser;
+
+use crate::cli::ArgType;
 
 fn main() {
     // Check CLI arguments
     let args = cli::parse_args();
 
     // Check the file type
+    let arg_type = args.get_arg_type();
     let arg_path = args.get_arg_path();
 
     // Create cursor over input file contents
-    let axml_cursor = create_cursor_from_apk(&arg_path);
+    let axml_cursor = match arg_type {
+        ArgType::Apk  => { create_cursor_from_apk(&arg_path)  },
+        ArgType::Axml => { create_cursor_from_axml(&arg_path) },
+        _ => todo!()
+    };
 
     // Parse the XML
     let elements = parser::parse_xml(axml_cursor);
