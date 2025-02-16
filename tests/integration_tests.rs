c83621668d38fe8befed3936e53abcc14304c459
Julien Gamba <julien@jgamba.eu>
2025-02-16 22:39:43 +0100

    Clippy

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,27 +1,27 @@
 mod consts;
 
 fn test_manifest_contents(axml: &rusty_axml::parser::Axml) {
-    let mut activities = rusty_axml::get_activities_names(&axml);
+    let mut activities = rusty_axml::get_activities_names(axml);
     activities.sort();
     assert_eq!(consts::ACTIVITIES, *activities);
 
-    let mut services = rusty_axml::get_services_names(&axml);
+    let mut services = rusty_axml::get_services_names(axml);
     services.sort();
     assert_eq!(consts::SERVICES, *services);
 
-    let mut providers = rusty_axml::get_providers_names(&axml);
+    let mut providers = rusty_axml::get_providers_names(axml);
     providers.sort();
     assert_eq!(consts::PROVIDERS, *providers);
 
-    let mut receivers = rusty_axml::get_receivers_names(&axml);
+    let mut receivers = rusty_axml::get_receivers_names(axml);
     receivers.sort();
     assert_eq!(consts::RECEIVERS, *receivers);
 
-    let mut requested_permissions = rusty_axml::get_requested_permissions(&axml);
+    let mut requested_permissions = rusty_axml::get_requested_permissions(axml);
     requested_permissions.sort();
     assert_eq!(consts::REQUESTED_PERMISSIONS, *requested_permissions);
 
-    let mut declared_permissions = rusty_axml::get_declared_permissions(&axml);
+    let mut declared_permissions = rusty_axml::get_declared_permissions(axml);
     declared_permissions.sort();
     assert_eq!(consts::DECLARED_PERMISSIONS, *declared_permissions);
 }
