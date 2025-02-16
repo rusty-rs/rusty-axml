af738abcd287fab7ef5f7cf64bd6f60bb909df29
Julien Gamba <julien@jgamba.eu>
2025-02-16 22:38:22 +0100

    Note

--- src/parser.rs
+++ src/parser.rs
@@ -354,13 +354,13 @@ pub fn parse_xml(mut axml_cursor: Cursor<Vec<u8>>) -> Result<Axml, AxmlError> {
     let mut global_strings = Vec::new();
     let mut namespace_prefixes = HashMap::<String, String>::new();
 
+    // TODO this will not work for non manifest files
     let root = Rc::new(RefCell::new(XmlElement {
         element_type: "manifest".to_string(),
         attributes: HashMap::new(),
         children: Vec::new()
     }));
     let mut stack = vec![Rc::clone(&root)];
-    // let mut stack: Vec<Rc<RefCell<XmlElement>>> = Vec::new();
 
     while let Ok(block_type) = ChunkType::parse_block_type(&mut axml_cursor) {
         match block_type {
